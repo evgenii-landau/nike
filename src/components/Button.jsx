31c83c32e4084d463ad135fd29f796ce31d8f915
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-29 09:01:09 +0300

    special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
-export const Button = ({iconUrl, label}) => {
+export const Button = ({iconUrl, label, backgroundColor, textColor, borderColor}) => {
 	return (
-		<button className='flex justify-center items-center gap-2 px-8 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+		<button className={`flex justify-center items-center gap-2 px-8 py-4 border font-montserrat text-lg leading-none
+		${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : `bg-coral-red rounded-full text-white border-coral-red`}`}>
 			{label}
 
 			{iconUrl && <img src={iconUrl} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5'/>}
