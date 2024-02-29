b0d83562d5515385d97101aa11e2728ea909f8d5
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-29 08:34:04 +0300

    about-us section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
 	const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
 
 	return (
-		<section id='#home' className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
+		<section id='home' className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
 			<div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
 				<p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
 				<h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,3 +1,20 @@
+import {products} from "../constants/index.js";
+import {PopularProductCard} from "../components/PopularProductCard.jsx";
+
 export const PopularProducts = () => (
-	<div>PopularProducts</div>
+	<section id='products' className='max-container nax-sm:mt-12'>
+		<div className='flex flex-col justify-start gap-5'>
+			<h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
+			<p className='lg:max-w-lg font-montserrat text-slate-gray'>
+				Experience top-notch quality and style with por sought-after selections. Discover a world of comfort, design,
+				and value
+			</p>
+		</div>
+
+		<div className='mt-16 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'>
+			{products.map((product) => (
+				<PopularProductCard key={product.name} {...product}/>
+			))}
+		</div>
+	</section>
 )
