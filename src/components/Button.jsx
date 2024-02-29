cbed8dd3cf9416f0dd67710a1b0865d21170257c
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-29 09:45:24 +0300

    footer section and deploy website

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-export const Button = ({iconUrl, label, backgroundColor, textColor, borderColor}) => {
+export const Button = ({iconUrl, label, backgroundColor, textColor, borderColor, fullWidth}) => {
 	return (
 		<button className={`flex justify-center items-center gap-2 px-8 py-4 border font-montserrat text-lg leading-none
-		${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : `bg-coral-red rounded-full text-white border-coral-red`}`}>
+		${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : `bg-coral-red rounded-full text-white border-coral-red ${fullWidth && 'w-full'}`}`}>
 			{label}
 
 			{iconUrl && <img src={iconUrl} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5'/>}
